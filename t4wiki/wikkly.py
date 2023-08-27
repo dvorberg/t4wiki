489f791547b7fb7d1d78457ea73973c3733e2d97
Diedrich Vorberg <diedrich@tux4web.de>
2023-08-27 21:58:17 +0200

    Use new macro baseclass.

--- t4wiki/wikkly.py
+++ t4wiki/wikkly.py
@@ -3,20 +3,9 @@ from io import StringIO
 
 from tinymarkup.context import Context
 from tinymarkup.macro import MacroLibrary
-from wikklytext.macro import ( WikklyMacro as Macro,
-                              ClassMacro, LanguageMacro, )
+from wikklytext.macro import ( WikklyMacro as Macro, WikklySource,
+                               LanguageMacro, ClassMacro, )
 from wikklytext.to_html import HTMLCompiler, to_html, to_inline_html
-
-# Bootstrap tables need the “table” CSS class and I don’t believe in
-# coding bootstrap specifics into my wikkly converter. So it came down to
-# this:
-old_open = HTMLCompiler.open
-def open(self, tag, **params):
-    if tag == "table" and not "class" in params:
-        params["class"] = "table"
-    old_open(self, tag, **params)
-HTMLCompiler.open = open
-
 
 class German(LanguageMacro):
     name = "de"
@@ -39,15 +28,10 @@ class NamedLanguageMacro(Macro):
     def tag_params(self, lang):
         return { "lang": lang }
 
-    def block_html(self, lang, contents:to_html):
-        return f'<div lang="{lang}">{contents}</div>'
-
-    def inline_html(self, lang, contents:to_inline_html):
-        return f'<span lang="{lang}">{contents}</span>'
-
-
 class toc(Macro):
-    def block_html(self):
+    environments = { "block" }
+
+    def html_element(self):
         return "<!--INHALT-->"
 
 class subdued(ClassMacro):
@@ -69,73 +53,83 @@ class noinclude(ClassMacro):
     pass
 
 class include(Macro):
-    def block_html(self, document_title):
+    environments = { "block" }
+
+    def html_element(self, document_title):
         return '<p><b style="color:red">INCLUDE: {document_title}</b></p>'
 
 class flashcard(Macro):
-    def block_html(self, titel, inhalt):
+    environments = { "block" }
+
+    def html_element(self, titel, inhalt):
         return ""
 
 class bselk(Macro):
-    def inline_html(self, value):
-        return "BSELK {value}"
+    environments = { "inline" }
+
+    def html_element(self, value):
+        return f"BSELK {value}"
 
 class hebrew(Macro):
-    def inline_html(self, value):
+    def html_element(self, value):
         return f"<code>transcode this: {value}</code>"
 
 class blockquote(Macro):
-    def block_html(self, contents:to_html):
+    environments = { "block" }
+    def html_element(self, contents:WikklySource):
         return f'<blockquote>{contents}</blockquote>'
 
 class anchor(Macro):
-    def inline_html(self, name):
+    def html_element(self, name):
         return f'<a name="{name}"></a>'
-
-    block_html = inline_html
 
 class Bild(Macro):
     name = "bild"
 
-    def block_html(self, filename):
-        return f'<figure><img src="{filename}" /></figure>'
+    def html_element(self, filename):
+        if self.environment == "block":
+            return f'<figure><img src="{filename}" /></figure>'
+        else:
+            return f'<img src="{filename}" />'
 
 class BildRechts(Macro):
     name = "bildrechts"
 
-    def block_html(self, filename):
-        return f'<figure><img src="{filename}" /></figure>'
-
-    def inline_html(self, filename):
+    def html_element(self, filename):
         return f'<img src="{filename}" />'
 
 class HTMLMacro(Macro):
     name = "html"
+    environment = { "block" }
 
-    def block_html(self, html:str):
+    def html_element(self, html:str):
         # tidy me??
         return html
-
-    inline_html = block_html
 
 class striped(ClassMacro):
     def tag_params(self):
         return { "class": "table table-striped" }
 
 class bibtex(Macro):
-    def block_html(self, contents):
+    environments = { "block" }
+
+    def html_element(self, contents):
         return '<pre>' + contents + '</pre>'
 
 class clear(Macro):
-    def block_html(self):
+    environments = { "block" }
+
+    def html_element(self):
         return '<div style="clear:both"></div>'
 
 class downloads(Macro):
-    def block_html(self):
+    environments = { "block" }
+    def html_element(self):
         return '<div><b>DOWNLOADS</b></div>'
 
 class poem(Macro):
-    def block_html(self, poem):
+    environments = { "block" }
+    def html_element(self, poem):
         return '<pre>{poeam}</pre>'
 
 
