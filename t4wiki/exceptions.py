7d62ec63a99e336f21b7a9c6c1a7b3cd170ce1e7
Diedrich Vorberg <diedrich@tux4web.de>
2023-09-01 16:55:39 +0200

    Added __str__() to Title.

--- t4wiki/exceptions.py
+++ t4wiki/exceptions.py
@@ -5,3 +5,7 @@ class TitleUnavailable(WikiException):
     def __init__(self, message, existing_titles):
         super().__init__(message)
         self.existing_titles = existing_titles
+
+    def __str__(self):
+        titles = [ et.title for et in self.existing_titles ]
+        return super().__str__() + " " + repr(titles)
