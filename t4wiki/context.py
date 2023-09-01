9d2b84557f4d8fba8f1cc0f21f8efeaf9a3ba9a1
Diedrich Vorberg <diedrich@tux4web.de>
2023-09-01 13:44:41 +0200

    Initial commit.

---
+++ t4wiki/context.py
@@ -0,0 +1,20 @@
+from flask import current_app as app
+from tinymarkup.context import Context
+
+from ll.xist.ns import html
+
+from .utils import citextset
+
+languages = app.config["LANGUAGES"]
+
+class Context(Context):
+    def __init__(self, macro_library, user_info):
+        super().__init__(macro_library, languages)
+        self.user_info = user_info
+        self.article_links = citextset()
+        self.article_includes = citextset()
+        self.macro_info = {}
+
+    def html_link_element(self, target, text):
+        self.article_links.add(target)
+        return super().html_link_element(target, text)
