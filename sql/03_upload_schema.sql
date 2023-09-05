511d02d4e8f2e2c9afb472d7b27f0bde285e2e13
Diedrich Vorberg <diedrich@tux4web.de>
2023-09-05 20:20:34 +0200

    Added BibTeX and Authentication; started on files.

---
+++ sql/03_upload_schema.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+DROP SCHEMA IF EXISTS uploads CASCADE;
+CREATE SCHEMA uploads;
+
+set search_path = uploads, wiki, public;
+
+CREATE TABLE upload
+(
+    id SERIAL PRIMARY KEY,
+    article_id INTEGER NOT NULL REFERENCES article ON DELETE CASCADE,
+    filename TEXT NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    gallery BOOLEAN NOT NULL DEFAULT false,
+    download BOOLEAN NOT NULL DEFAULT false,
+    sortrank FLOAT NOT NULL DEFAULT 0.0,
+    width INTEGER,
+    height INTEGER,
+    data BYTEA NOT NULL,
+    url TEXT,
+    ctime TIMESTAMP NOT NULL DEFAULT NOW(),
+    slug CHAR(20) NOT NULL DEFAULT slug(),
+
+    UNIQUE(slug)
+);
+
+COMMIT;
