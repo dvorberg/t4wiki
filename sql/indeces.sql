2eff2b5a58793b395155ddab2fde0b27b537264d
Diedrich Vorberg <diedrich@tux4web.de>
2023-09-07 22:59:24 +0200

    Initial commit.

---
+++ sql/indeces.sql
@@ -0,0 +1,36 @@
+BEGIN;
+
+set search_path = wiki, public;
+
+CREATE INDEX article_fulltitle_index ON article_title
+((
+    CASE
+      WHEN namespace IS NULL THEN title
+      WHEN namespace IS NOT NULL THEN (
+         (title || ' (') || namespace) || ')'
+    END::citext
+));
+
+CREATE INDEX article_bibtex_key ON article(bibtex_key);
+
+CREATE INDEX article_fulltext ON article USING GIN (tsvector);
+
+
+CREATE INDEX article_bibref_article_id ON article_bibref(article_id);
+CREATE INDEX article_include_article_id ON article_include(article_id);
+CREATE INDEX article_link_article_id ON article_link(article_id);
+CREATE INDEX article_title_article_id ON article_title(article_id);
+
+
+set search_path = archive, public;
+
+CREATE INDEX article_revision_article_id ON article_revision(article_id);
+
+
+set search_path = uploads, public;
+
+CREATE INDEX upload_article_id ON upload(article_id);
+
+
+
+COMMIT;
