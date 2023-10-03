58a5f23b98536d7be4e74883bfe8017fcbe5cf67
Diedrich Vorberg <diedrich@tux4web.de>
2023-10-03 20:37:26 +0200

    Removed.

--- sql/indexes.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-CREATE UNIQUE INDEX article_fulltitle_index ON wiki.article_title ((
-      CASE WHEN namespace IS NULL THEN title
-           WHEN namespace IS NOT NULL THEN title || ' (' || namespace || ')'
-      END    
-));
-
-COMMIT;
