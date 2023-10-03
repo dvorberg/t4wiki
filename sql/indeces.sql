ae144942a917b41caef4b32ba30e6a7d3819f912
Diedrich Vorberg <diedrich@tux4web.de>
2023-10-03 20:39:16 +0200

    Used generated columns.

--- sql/indeces.sql
+++ sql/indeces.sql
@@ -2,19 +2,10 @@ BEGIN;
 
 set search_path = wiki, public;
 
-CREATE INDEX article_fulltitle_index ON article_title
-((
-    CASE
-      WHEN namespace IS NULL THEN title
-      WHEN namespace IS NOT NULL THEN (
-         (title || ' (') || namespace) || ')'
-    END::citext
-));
-
+CREATE INDEX article_fulltitle_index ON article_title(full_title);
 CREATE INDEX article_bibtex_key ON article(bibtex_key);
 
-CREATE INDEX article_fulltext ON article USING GIN (tsvector);
-
+CREATE INDEX article_fulltext ON article_rendition USING GIN (tsvector);
 
 CREATE INDEX article_bibref_article_id ON article_bibref(article_id);
 CREATE INDEX article_include_article_id ON article_include(article_id);
@@ -30,7 +21,5 @@ CREATE INDEX article_revision_article_id ON article_revision(article_id);
 set search_path = uploads, public;
 
 CREATE INDEX upload_article_id ON upload(article_id);
-
-
 
 COMMIT;
