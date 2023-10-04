89a795632e70671edd28cbd59513e05499635f59
Diedrich Vorberg <diedrich@tux4web.de>
2023-10-05 00:45:29 +0200

    Added on_resize handler and serving stretch-to-bottom class.

--- skin/t4wiki.mjs
+++ skin/t4wiki.mjs
@@ -15,5 +15,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelectorAll("form input").forEach(id_to_name);
     document.querySelectorAll("form textarea").forEach(id_to_name);
     document.querySelectorAll("form select").forEach(id_to_name);
+
+	function on_resize(event)
+	{
+		const div = document.querySelector(".stretch-to-bottom");
+		if (div)
+		{
+			const r = div.getBoundingClientRect(),
+				  mb = parseInt(div.getAttribute("data-margin-bottom")) || 120,
+				  h = window.innerHeight - r.y - mb;
+			if (h > 0)
+			{
+				div.style.height = h + "px";
+			}
+			
+		}
+	}
+
+	on_resize();
+	window.addEventListener("resize", on_resize);
 });
 
