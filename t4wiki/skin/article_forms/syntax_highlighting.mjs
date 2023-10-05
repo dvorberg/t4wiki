86f536aeaae6c1db80eca6f6c4a12d9d5c6ce94f
Diedrich Vorberg <diedrich@tux4web.de>
2023-10-05 16:15:05 +0200

    Moved here.

---
+++ t4wiki/skin/article_forms/syntax_highlighting.mjs
@@ -0,0 +1,87 @@
+import {CodeJar} from 'codejar';
+import { Prism } from 'prismjs';
+
+Prism.languages.bibtex = {
+	'comment': /%.*/,
+	
+	/*
+	 * section or chapter headlines are highlighted as bold so that
+	 * they stand out more
+	 */
+	'entry-type': {
+		pattern: /@[a-z]+/
+	},
+	
+	'citekey': {
+		pattern: /(\{)(\w+)/,
+		lookbehind: true
+	},
+
+	'string': {
+		pattern: /".*"/,
+		greedy: false
+	},
+	
+	'punctuation': /[[\]{}&]/
+};
+
+Prism.languages.wikkly = {
+	'comment': {
+		pattern: /\/%.*%\//,
+		greedy: false
+	},
+	
+	'wikkly-footnote': {
+		pattern: /<<footnote.*>>/,
+		greedy: false
+	},
+
+	'wikkly-italic': {
+		pattern: /\/\/[^\/]*\/\//,
+		greedy: false
+	},
+
+	'wikkly-bold': {
+		pattern: /''[^']*''/,
+		greedy: false
+	},
+
+	'wikkly-heading': {
+		pattern: /(^|\n)!+\s*.*/		
+	},
+
+	'wikkly-enumeration': {
+		pattern: /(^|\n)[#•\*]+/
+	},
+	
+	'macro': {
+		pattern: /(<<)[a-zA-Z]+\s+/,
+		lookbehind: true,
+		alias: 'variable'
+	},
+
+	'link': {
+		pattern: /(\[\[)[^\]]*(?=\]\])/,
+		greedy: false,
+		lookbehind: true
+	},
+	
+	'punctuation': /[[\]{}&"']|<<|>>/
+};
+
+
+export function init_editor(node) {
+	const textarea = node.querySelector("textarea"),
+		  viewer = node.querySelector(".source-viewer");
+
+	viewer.textContent = textarea.value;
+
+	let jar = CodeJar(viewer, function(node) {
+		Prism.highlightElement(node, false);
+	}, { addClosing: false, catchTab: false });
+
+	jar.onUpdate(code => {
+		textarea.value = viewer.textContent;
+	});
+}
+
