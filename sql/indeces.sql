eb20a6e9699a6d4702761bcdfef81d9c7e8b20f0
Diedrich Vorberg <diedrich@tux4web.de>
2023-10-06 01:10:47 +0200

    Fixed

--- sql/indeces.sql
+++ sql/indeces.sql
@@ -5,13 +5,13 @@ set search_path = wiki, public;
 CREATE INDEX article_fulltitle_index ON article_title(full_title);
 CREATE INDEX article_bibtex_key ON article(bibtex_key);
 
-CREATE INDEX article_fulltext ON article_rendition USING GIN (tsvector);
+CREATE INDEX article_fulltext ON article USING GIN (tsvector);
 
 CREATE INDEX article_bibref_article_id ON article_bibref(article_id);
 CREATE INDEX article_include_article_id ON article_include(article_id);
 CREATE INDEX article_link_article_id ON article_link(article_id);
 CREATE INDEX article_title_article_id ON article_title(article_id);
-
+CREATE INDEX article_title_namespace ON article_title(namespace);
 
 set search_path = archive, public;
 
