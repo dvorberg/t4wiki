6dd7e68d9eaa7281906b6931ea64139daaa08cf3
Diedrich Vorberg <diedrich@tux4web.de>
2024-12-11 23:44:51 +0100

    Images have no src= attribute to avoid redundant requests.

--- t4wiki/macrotools.py
+++ t4wiki/macrotools.py
@@ -3,14 +3,14 @@ from ll.xist.ns import html
 
 def block_level_figure(image_filename, description):
     return html.figure(
-        html.img(src=image_filename,
-                 class_="rounded preview-image preview-1800"),
+        html.img(class_="rounded preview-image preview-1800",
+                 **{"data-filename": image_filename}),
         html.figcaption(description, class_="figure-caption"),
         class_="figure t4wiki-figure")
 
 def float_right_image(image_filename, description):
     return html.figure(
-        html.img(src=image_filename,
-                 class_="rounded preview-image preview-300"),
+        html.img(class_="rounded preview-image preview-300",
+                 **{"data-filename": image_filename}),
         html.figcaption(description, class_="figure-caption"),
         class_="figure t4wiki-figure float-end small")
