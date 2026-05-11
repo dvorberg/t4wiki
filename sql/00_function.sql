bc04e00c9d74563b9f62e6b66cee07f77ce99ca7
Diedrich Vorberg <diedrich@tux4web.de>
2026-05-11 12:24:30 +0200

    Added html_wordcount().

--- sql/00_function.sql
+++ sql/00_function.sql
@@ -5,7 +5,7 @@ set search_path = public;
 CREATE OR REPLACE FUNCTION plpython3_call_handler()
    RETURNS language_handler AS '$libdir/plpython3.so' LANGUAGE 'c';
 
-CREATE OR REPLACE LANGUAGE 'plpython' HANDLER plpython3_call_handler;
+-- CREATE OR REPLACE LANGUAGE 'plpython' HANDLER plpython3_call_handler;
 
 
 CREATE OR REPLACE FUNCTION public.update_mtime() RETURNS trigger AS'
@@ -28,6 +28,31 @@ CREATE OR REPLACE FUNCTION title_to_cmpid(title TEXT) RETURNS TEXT IMMUTABLE
 AS $$
 from t4.title_to_id import title_to_id
 return title_to_id(title)
+$$ LANGUAGE plpython;
+
+
+CREATE OR REPLACE FUNCTION html_wordcount(html TEXT) RETURNS INTEGER IMMUTABLE
+AS $$
+import re
+
+br_re = re.compile(r"<br[^>]*>")
+tag_re = re.compile(r"<[^>]+>")
+whitespace_re = re.compile(r"\s+")
+
+def wordcount(html:str|None) -> int:
+    if html is None:
+        return 0
+        
+    html = html.strip()
+
+    if not html:
+        return 0
+
+    html = br_re.sub(" ", html)
+    html = tag_re.sub("", html)
+    return len(whitespace_re.split(html))
+
+return wordcount(html)
 $$ LANGUAGE plpython;
 
 
